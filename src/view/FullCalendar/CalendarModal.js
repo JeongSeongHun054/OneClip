a8bb4060ac310a27f22ebfd427e45734d730bde2
JeongSeongHun054 <gnsl1465@naver.com>
2020-11-27 10:20:23 +0900

    Calendar 완성

--- src/view/FullCalendar/CalendarModal.js
+++ src/view/FullCalendar/CalendarModal.js
@@ -32,8 +32,7 @@ const ModalInner = styled.div`
   box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.5);
   background-color: #fff;
   border-radius: 10px;
-  width: 360px;
-  max-width: 480px;
+  width: 450px;
   top: 50%;
   transform: translateY(-50%);
   margin: 0 auto;
--- src/view/FullCalendar/FullCalendar.js
+++ src/view/FullCalendar/FullCalendar.js
@@ -5,23 +5,47 @@ import dayGridPlugin from "@fullcalendar/daygrid"; // needed for dayClick
 import interactionPlugin from "@fullcalendar/interaction";
 import CalendarModal from "./CalendarModal";
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  input {
+    height: 45px;
+    padding: 1rem;
+    border-radius: 0.2rem;
+    border: 1px solid #d4d5d6;
+    color: #565656;
+    font-size: 20px;
+
+    &:not(:last-child) {
+      margin-bottom: 20px;
+    }
+  }
+
+  input[type="submit"] {
+    color: #fff;
+    background-color: #38afd1;
+    border-color: #38adf1;
+    box-shadow: 0 2px 6px 0 rgba(57, 175, 209, 0.5);
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+      border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out,
+      -webkit-box-shadow 0.15s ease-in-out;
+    &:hover {
+      background-color: #2b99b9;
+      border-color: #2991ae;
+    }
+  }
+`;
+
 const Calendar = () => {
   const [todos, setTodos] = useState([]);
-
-  const [inputValue, setInputValue] = useState({
-    title: "",
-    date: "",
-  });
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
 
   const onChange = (e) => {
-    setInputValue({
-      ...inputValue,
-      [e.target.name]: e.target.value,
-    });
+    setTitle(e.target.value);
   };
 
   const Add = () => {
-    const { title, date } = inputValue;
     setTodos(
       todos.concat({
         title,
@@ -34,23 +58,27 @@ const Calendar = () => {
     e.preventDefault();
     Add();
     closeModal();
-    setInputValue({
-      title: "",
-      date: "",
-    });
-    console.log(todos);
   };
 
   const [modalVisible, setModalVisible] = useState(false);
 
-  const openModal = () => {
+  const openModal = (e) => {
     setModalVisible(true);
+    setDate(e.dateStr);
   };
   const closeModal = () => {
     setModalVisible(false);
+    setTitle("");
+    setDate("");
   };
   return (
     <>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        dateClick={openModal}
+        events={todos}
+      />
+
       {modalVisible && (
         <CalendarModal
           visible={modalVisible}
@@ -58,29 +86,25 @@ const Calendar = () => {
           maskClosable={true}
           onClose={closeModal}
         >
-          <form onSubmit={onSubmit}>
+          <Form onSubmit={onSubmit}>
             <input
               type="text"
               name="title"
-              value={inputValue.title}
+              value={title}
               onChange={onChange}
+              autoFocus
             />
             <input
               type="text"
               name="date"
-              value={inputValue.date}
+              value={date}
               onChange={onChange}
+              style={{ textAlign: "center" }}
             />
-            <input type="submit" />
-          </form>
+            <input type="submit" value="등록" />
+          </Form>
         </CalendarModal>
       )}
-
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        dateClick={openModal}
-        events={todos}
-      />
     </>
   );
 };
